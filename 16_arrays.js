af5ba392b51cf1c89c8d0a14da1b896f519b51dd
Rahul <dasr16583@gmail.com>
2023-01-15 21:33:41 +0530

    Arrays

---
+++ 16_arrays.js
@@ -0,0 +1,18 @@
+// arrays:- Arrays are the collection of elements
+// strings are immutuable strings cannot be changecan be change
+// Arrays are mutuable arrays 
+let marks = [20,21,22,25,55]
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+
+// Adding a new value in array
+marks[5] = 90;
+// change the value of an array
+marks[0]=99;
+
+// kength property
+console.log(`The length of marks is : `,marks);
+console.log(marks);
+
