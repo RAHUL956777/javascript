e6fb5a28b0269f5e9114a9697e8a5a1ba65ab730
Rahul <dasr16583@gmail.com>
2023-01-20 23:09:07 +0530

    exericies

---
+++ 22_GaussTheNumberPratice.js
@@ -0,0 +1,9 @@
+/* write a javascript program to generate a random number and store it in a  variable.
+The program then takes an input from the users to tell them wheateher the gauss was correct
+, greatest or lowest to them the original number
+100b--(number of gauss) is the score of the users The program is excepted to terminal
+once the number is gaused shall be between 0 to 100*/ 
+
+let a = Math.floor((Math.random()*100)+0);
+
+
