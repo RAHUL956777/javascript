70fe41a2df826ecdd161a73dabab6c729ddc97bc
Rahul <dasr16583@gmail.com>
2023-01-27 20:47:29 +0530

    Element inly navigation

---
+++ element_only_navigation/script.js
@@ -0,0 +1,11 @@
+const changeBodyRed = ()=>{
+    document.body.firstElementChild.style.background = "red";
+}
+
+
+let b = document.body;
+console.log("The  first child of b is: ", b.firstChild);
+console.log("First element child of b is: ", b.firstElementChild);
+
+// let ch = $0.children;
+// console.log(ch);
