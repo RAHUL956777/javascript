39e7b950178a8e901c9bcd87420a43a6d7cf25df
Rahul <dasr16583@gmail.com>
2023-02-04 23:07:58 +0530

    Generating a qr code of a text or link ushing javascript

---
+++ automatedQRgenerator/script.js
@@ -0,0 +1,6 @@
+const wrap = document.querySelector(".wrap");
+const generateBtn = wrap.querySelector(".form button");
+
+generateBtn.addEventListener("click", () => {
+  wrap.classList.add("active");
+});
