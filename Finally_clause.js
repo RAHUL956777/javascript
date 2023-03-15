be740335d203c8a78ea25a68e5d4b93d6e34e0c4
Rahul <dasr16583@gmail.com>
2023-03-15 19:48:39 +0530

    Finaly clause in javascript

---
+++ Finally_clause.js
@@ -0,0 +1,13 @@
+const f = () => {
+  try {
+    let a = 5;
+    console.log(program);
+    console.log("Program run sucessfully");
+  } catch {
+    console.log("This is an error");
+    console.log(p);
+  } finally {
+    console.log("I am a good boy");
+  }
+};
+f();
