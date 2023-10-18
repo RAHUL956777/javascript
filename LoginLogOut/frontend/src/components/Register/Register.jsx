358c631a4ad4412a48426541dab8297181d50b09
RAHUL <dasr16583@gmail.com>
2023-10-18 08:37:37 +0530

    working on register component

--- LoginLogOut/frontend/src/components/Register/Register.jsx
+++ LoginLogOut/frontend/src/components/Register/Register.jsx
@@ -1,29 +1,62 @@
+import "./Register.css";
 import { useState } from "react";
 
 const Register = () => {
-  const [value,setValue] = useState("");
   
+  const [user, setUser] = useState({
+    name:"",
+    email:"",
+    password:"",
+    reEnterPassword:"",
+  });
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setUser({
+      ...user,
+      [name]:value,
+    })
+  };
+
+  const subMitBtn = () => {};
+
   return (
     <>
       <h1>Register</h1>
       <div className="form">
-        <input type="text" placeholder="Please enter your name"
-        value={value}
-        onChange={(e)=> setValue(e.target.value)}
+        <input
+          type="text"
+          placeholder="Please enter your name"
+          value={user.name}
+          onChange={handleChange}
         />
-        <input type="email" placeholder="Eniter your email"
-        value={value}
-        onChange={(e)=> setValue(e.target.value)}/>
-        <input type="password" placeholder="Enter your password"
-        value={value}
-        onChange={(e)=> setValue(e.target.value)}/>
-        <input type="password" placeholder="Confirm your password"
-        value={value}
-        onChange={(e)=> setValue(e.target.value)}/>
-        <button type="submit" onClick={subMitBtn}>Register</button>
+        <input
+          type="email"
+          placeholder="Eniter your email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Confirm your password"
+          value={user.reEnterPassword}
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={subMitBtn}>
+          Register
+        </button>
+        <button type="submit" >
+          Login
+        </button>
       </div>
     </>
-  )
+  );
 };
 
 export default Register;
